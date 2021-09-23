f6c9ac3b622f6324f5473909d4e54f51d39ec840
unknown <puvvadisreevineetha@gmail.com>
2021-09-23 11:37:58 -0700

    modifed mavendemo2

--- src/test/java/mavendemo2/AppTest.java
+++ src/test/java/mavendemo2/AppTest.java
@@ -1,14 +1,12 @@
 package mavendemo2;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class AppTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+	       System.out.println("test");
 	}
 
 }
